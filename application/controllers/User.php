17b84f47499effa151a9cfbf5a2aa87cdc9b3732
John Cris <johncrismanabo9@gmail.com>
2022-05-25 13:12:40 +0800

    get all user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -17,6 +17,23 @@ class User extends CI_Controller {
         $data['page_title'] = "User"; 
         $this->load->view('admin/user', $data); 
 	}
+
+	public function get_all_user()
+	{ 
+		$user = $this->user_model->get_all_user(); 
+		foreach( $user  as $row ){ 
+            if($row['role_type'] == 1){
+                $row['role_type'] = "Admin";
+            }else if($row['role_type'] == 2){
+                $row['role_type'] = "User";
+            }
+            $row['password'] = null;
+			$data['data'][] = $row; 
+
+		} 
+		echo json_encode($data); 
+	
+	}
  
         
 }
