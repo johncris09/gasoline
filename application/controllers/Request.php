2da252b4f8a853df2ad57e19fb7caeecf7e2ad00
John Cris <johncrismanabo9@gmail.com>
2022-05-25 09:05:39 +0800

    get all request

--- application/controllers/Request.php
+++ application/controllers/Request.php
@@ -17,6 +17,21 @@ class Request extends CI_Controller {
         $data['page_title'] = "Request"; 
         $this->load->view('admin/request', $data); 
 	}
+
+    
+	public function get_all_request()
+	{   
+		$request = $this->request_model->get_all_request(); 
+		foreach( $request  as $row ){
+            $date = date('m/d/Y', strtotime($row['request_date']));
+            $name = ucwords($row['lastname'] . "," . $row['firstname'] . " " . $row['middlename']  . " " . $row['suffix'] );
+			$row['name'] = $name;
+            $row['request_date'] = $date;
+			$data['data'][] = $row;
+		} 
+		echo json_encode($data); 
+	
+	}
  
  
         
