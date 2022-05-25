29e53d6df6e77937949daa651e644da57bb98138
John Cris <johncrismanabo9@gmail.com>
2022-05-25 12:41:39 +0800

    create new custom config

---
+++ application/config/custom_config.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+$config['gasoline_type'] = [
+    'Regular Gasoline',
+    'Special Gasoline',
+    'Diesel',
+    'Kerosine',
+];
