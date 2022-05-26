ef657d6db6b242dcc8d9d7b83cfec662f6c9d864
John Cris <johncrismanabo9@gmail.com>
2022-05-26 08:22:02 +0800

    order by rquest date desc

--- application/models/Request_model.php
+++ application/models/Request_model.php
@@ -11,9 +11,10 @@ class Request_model extends CI_Model
 
     public function get_all_request()
     {   
-        return $this->db 
+        return $this->db
             ->where('request.driver = driver.id')
             ->where('request.plate_number = vehicle_type.id')
+            ->order_by('request.request_date', 'desc')
 			->get('request`, driver, vehicle_type')
 			->result_array();
     }
