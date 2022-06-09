639c8369a4b5942b973a86a4c5c77547c463816a
John Cris <johncrismanabo9@gmail.com>
2022-06-09 16:14:51 +0800

    init

---
+++ application/models/Receipt_model.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Receipt_model extends CI_Model
+{
+	public $table = "trip_ticket";
+    public function __construct()
+    {
+        parent::__construct();
+    } 
+
+    
+
+}
