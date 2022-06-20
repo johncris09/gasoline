52e29b1f56e739407c8dda5950a2f285af3596ee
John Cris <johncrismanabo9@gmail.com>
2022-06-20 13:00:58 +0800

    view summary by date

--- application/controllers/Summary.php
+++ application/controllers/Summary.php
@@ -17,6 +17,31 @@ class Summary extends CI_Controller {
         $data['page_title'] = "Summary"; 
         $this->load->view('admin/summary', $data); 
 	}
+
+    
+    public function view()
+	{   
+        $date = array(
+            'from' => date('Y-m-d', strtotime($_GET['from'])),
+            'to' => date('Y-m-d', strtotime($_GET['to'])),
+        );
+        $view_summary = $this->receipt_model->view_summary($date); 
+        
+        
+        if($view_summary->num_rows() > 0 ){
+            $data['summary'] = $view_summary->result_array();
+            $data['page_title'] = "View Summary";
+            $this->load->view('admin/view_summary', $data);
+        }else{
+            echo '
+                <script>
+                    alert("No Record Found");
+                    location.href="../report"
+                </script>
+            ';
+        } 
+         
+    }
  
       
 }    
