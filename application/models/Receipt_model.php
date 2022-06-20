1e8bf7d8a9623df4172cb490114a1216ac8cd0b6
John Cris <johncrismanabo9@gmail.com>
2022-06-20 13:04:36 +0800

    get receipt, update, view summary

--- application/models/Receipt_model.php
+++ application/models/Receipt_model.php
@@ -19,6 +19,37 @@ class Receipt_model extends CI_Model
             return true;
         }
     }
- 
+
+    
+    
+    public function get_receipt($data)
+    {  
+        $this->db->where($data);
+        $receipt =  $this->db->get($this->table);
+        if($receipt->num_rows() > 0)
+            return $receipt->result_array()[0];
+        
+    } 
+
+    
+    public function update($data)
+    { 
+        return $this->db
+            ->where('id', $data['id'])
+            ->update($this->table, $data);
+    }
+    
+    
+    public function view_summary($data)
+    {   
+        return $this->db
+            ->where('receipt.plate_number = vehicle_type.id')
+            ->where('date >= ', $data['from'])
+            ->where('date <= ', $data['to']) 
+            ->order_by('date', 'desc') 
+			->get('receipt, vehicle_type');
+    }
+
+
 
 }
